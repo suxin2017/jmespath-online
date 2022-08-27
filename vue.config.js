de5be6ee6968bc54c213b007800fddcf175e0685
suxin2017 <1107178482@qq.com>
2022-08-27 15:52:48 +0800

    fix: bug

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
-    publicPath: process.env.NODE_ENV === "production" ? "/jmespath-onlin/" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/jmespath-online/" : "/",
     transpileDependencies: true
 })
